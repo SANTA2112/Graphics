d0ef05666b5eaff5d4bc92eb77d1ad9e2bbbac45
Sergey Yushmanov <s.yushmanov777@gmail.com>
2018-04-10 19:09:28 +0700

    changed the path to the files

---
+++ js/graph.js
@@ -0,0 +1,30 @@
+Highcharts.chart('swiper-slide-graph1', {
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: ''
+  },
+  xAxis: {
+    categories: ['2016', '2017', '2018']
+  },
+  tooltip: {
+    pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+    shared: true,
+  },
+  plotOptions: {
+    column: {
+      stacking: 'percent'
+    }
+  },
+  series: [{
+    name: '1',
+    data: [3, 2.7, 2.5],
+  }, {
+    name: '2',
+    data: [6, 5.5, 5]
+  }, {
+    name: '3',
+    data: [1, 1.8, 2.5]
+  }]
+});
